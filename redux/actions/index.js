d64218fc4f788b1e8786b7dcf3883e4b1199b4cd
Pyramid Oscar <30281971+bhavikji@users.noreply.github.com>
2021-01-27 10:14:52 +0530

    Update index.js

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,6 +1,6 @@
 import authActions from './authActions';
 
-// this is for you chitraji, this ...authActions is a spread it is use to import all the function from the file for example, it will import all the actions created in authActions file.
+// this is for those who have doubt regarding spread operator, this ...authActions is a spread it is use to import all the function from the file for example, it will import all the actions created in authActions file.
 //Which means,
 /* exports default {
   authenticate,
